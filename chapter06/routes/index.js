476cd38ba21aabf0401cb1fbda4f5a1cfed57725
1xiyu <xiyu1smile@163.com>
2017-03-12 22:01:43 +0800

    fxfx

--- chapter06/routes/index.js
+++ chapter06/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var multiparty = require('multiparty');
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', name: 'litchi',username: req.session.username });
@@ -27,5 +29,35 @@ router.get('/logout',function(req,res){
 
 router.get('/comment',function(req,res){
   res.render('comment',{});
+})
+
+router.get('/uploadImg',function(req,res){
+  var form = new multiparty.Form();
+  // set encoding
+  form.encoding = 'utf-8';
+  //set file save path
+  form.uploadDir = './uploadtemp';
+  //set file size
+  form.maxFilesSize = 2 * 1024 * 1024;
+
+  form.parse(req,function(err,fields,files){
+    var uploadurl = '/images/upload/';
+    file = files['filedata'];
+    originalFilename = file[0].originalFilename;   // 原始文件名
+    tmpPath = file[0].path;
+
+    var timestamp = new Date().getTime();
+    uploadurl += timestamp + originalFilename;
+    newPath = './public/uploadurl';
+
+    var fileReadStream = fs.createReadStream('tmpPath');
+    var fileWriteStram = fs.createWriteStream(newPath);
+
+    fileReadStream.pipe(fileWriteStram);  // 管道流
+    fileWriteStram.on('close',function(){
+      fs.unlinkSync(tmpPath);
+      res.send('{"err":"","msg":"' + uploadurl + '"}');
+    })
+  })
 })
 module.exports = router;
